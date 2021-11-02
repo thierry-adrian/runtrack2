fb46ac139b58ee70a65cbd420b9796bea7782449
thiierry <thierry.adrian@laplateforme.io>
2021-11-02 16:07:43 +0100

    jour 1

---
+++ jour01/job01/index.php
@@ -0,0 +1,21 @@
+<?php
+$str = "La Plateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo "$str2 $str $str3";
+
+echo "<br>";
+$val = 6;
+echo "$val";
+echo "<br>";
+$ad = $val + 4;
+echo "$ad";
+
+echo "<br>";
+$myBool = true;
+echo "$myBool";
+echo "<br>";
+$myBool = false;
+echo "$myBool";
+
+?>
---
+++ jour01/job02/index.php
@@ -0,0 +1,5 @@
+<?php
+$str = "La Plateforme !";
+echo $str; // Ah ! La Plateforme. Pour travailler, c'est super.
+echo "<br>";
+echo $str; // Le futur centre névralgique, espérons-nous tous.
