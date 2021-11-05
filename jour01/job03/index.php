0c9c6a080e755acb769c64b1c76f959c426bd349
thiierry <thierry.adrian@laplateforme.io>
2021-11-05 13:59:12 +0100

    runtrack/02jour01/job03

---
+++ jour01/job03/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau PHP/HTML</title>
+</head>
+
+<body>
+    <table>
+        <ul>
+            <li>
+                <?php
+                    $prenom = 'Crocus';
+                    echo $prenom;
+                ?>
+            </li>
+            <li>
+                <?php
+                    $nb = 82027;
+                    echo $nb;
+                ?>
+            </li>
+            <li>
+                <?php
+                    $vr = 0.081;
+                    echo $vr;
+                ?>
+            </li>
+            <li>
+                <?php
+                    $bleu = false;
+                    echo $bleu;
+
+                ?>
+            </li>
+        </ul>
+    </table>    
+    <br>
+    <table>
+        <ol>
+            <li>
+                <?php
+                    $prenom = 'Crocus';
+                    echo $prenom;
+                ?>
+            </li>
+            <li>
+                <?php
+                    $nb = 82027;
+                    echo $nb;
+                ?>
+            </li>
+            <li>
+                <?php
+                    $vr = 0.081;
+                    echo $vr;
+                ?>
+            </li>
+            <li>
+                <?php
+                    $bleu = true;
+                    echo $bleu;
+                ?>
+            </li>
+        </ol>
+    </table>    
+</body>
+
+</html>
+
+
+
+    <?php
+/*      $prenom = 'Crocus';
+        $nb = 82027;
+        $vr = 0.081;
+        $bleu = false;  */
+    ?>
+
