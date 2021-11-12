18452a30581d2d13a082af51310ff78b7729d1fe
thiierry <thierry.adrian@laplateforme.io>
2021-11-12 10:01:18 +0100

    ajout du jour 2

---
+++ Jour02/Job01/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Liste des nombres compris entre 0 et 1337</h2>
+</body>
+</html>
+
+
+<?php
+
+// <Liste des nombres compris entre 0 et 1337.
+
+for ($i = 1; $i < 1337; $i++) {
+    
+if ($i == 42) {
+    echo "<b><u>".$i."</u></b></br>";
+} else {
+    echo $i;
+    echo '<br/>';
+}
+
+}
+
+for ($i = 0; $i > 0; $i = $i++){
+
+            // for ($i = 0; $i < 1337; $i++) {
+
+if ($i > 0 && )
+
+if ($i == 42){
+    echo "<b><u>".$i."</u></b></br>";
+}
+}   echo $i;
+    echo 'br/';
+}
+?>
